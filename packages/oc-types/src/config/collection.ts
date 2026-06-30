5bbaf8c6baf393bd1a706cac67567d7764e83213
bruno-sachin <bruno-sachin@brunos-MacBook-Air.local>
2026-06-30 18:48:05 +0530

    Removed scripts type from collection.ts file

--- packages/oc-types/src/config/collection.ts
+++ packages/oc-types/src/config/collection.ts
@@ -7,17 +7,10 @@ import type { Protobuf } from './protobuf';
 import type { Proxy } from './proxy';
 import type { Environment } from './environments';
 
-export type ScriptExecutionFlow = 'sandwich' | 'sequential';
-
-export interface CollectionScriptsConfig {
-  flow?: ScriptExecutionFlow;
-}
-
 export interface CollectionConfig {
   environments?: Environment[];
   protobuf?: Protobuf;
   proxy?: Proxy;
   clientCertificates?: ClientCertificate[];
-  scripts?: CollectionScriptsConfig;
 }
 
