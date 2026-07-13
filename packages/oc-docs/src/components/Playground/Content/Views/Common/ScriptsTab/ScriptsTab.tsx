de99298a30d34fe70e17dfc22523106fb94a9a72
Vineet Sharma <vineet@usebruno.com>
2026-07-13 16:41:16 +0530

    Improving styling

--- packages/oc-docs/src/components/Playground/Content/Views/Common/ScriptsTab/ScriptsTab.tsx
+++ packages/oc-docs/src/components/Playground/Content/Views/Common/ScriptsTab/ScriptsTab.tsx
@@ -35,28 +35,24 @@ export const ScriptsTab: React.FC<ScriptsTabProps> = ({
       id: 'pre-request',
       label: 'Pre request',
       content: (
-        <div className="mt-3">
           <CodeEditor
             value={scripts.preRequest || ''}
             onChange={(value: string) => onScriptChange('preRequest', value)}
             language="javascript"
             height="300px"
           />
-        </div>
       )
     },
     {
       id: 'post-response',
       label: 'Post response',
       content: (
-        <div className="mt-3">
           <CodeEditor
             value={scripts.postResponse || ''}
             onChange={(value: string) => onScriptChange('postResponse', value)}
             language="javascript"
             height="300px"
           />
-        </div>
       )
     }
   ];
