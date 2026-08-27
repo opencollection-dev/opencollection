1ef7416591b2b9e3fe7b2dbf2e758bc3a27ee276
morsh-bruno <morsh@usebruno.com>
2026-08-27 18:08:37 +0530

    feat: add grpc lifecycle hooks to ScriptType (#156)

--- packages/oc-types/src/common/scripts.ts
+++ packages/oc-types/src/common/scripts.ts
@@ -2,7 +2,15 @@
  * Scripts executed throughout the collection lifecycle
  */
 
-export type ScriptType = 'before-request' | 'after-response' | 'tests' | 'hooks';
+export type ScriptType =
+  | 'before-request'
+  | 'after-response'
+  | 'tests'
+  | 'hooks'
+  | 'grpc:before-call-start'
+  | 'grpc:after-call-end'
+  | 'grpc:before-message-send'
+  | 'grpc:after-message-receive';
 
 export interface Script {
   type: ScriptType;
